9169e7222e6e66b087a99451d3540c94ea6a2040
Amal-Az <amalazour430@gmail.com>
2025-05-05 18:14:38 +0100

    ex1

---
+++ week1/Day1/Exercises/Ex1.py
@@ -0,0 +1,19 @@
+# 1 
+first= "Hello World"*4
+print (first)
+2# 
+a= (99**3)*8
+print(a)
+# 3
+name= input("enter your name: ")
+if name == "amal":
+   print("ooh! we have the same name")
+else:
+   print("ooh! your name is so beautiful")
+# 4
+height= input(int("enter your height in centimeters: "))
+if height > 145:
+   print("your tall is enough to ride.")
+else:
+   print("you need to grow some more to ride.")
+# 5
