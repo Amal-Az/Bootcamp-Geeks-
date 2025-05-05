51ebfeaffdc1821a51b24284706e218afa4f8ce1
Amal-Az <amalazour430@gmail.com>
2025-05-06 00:41:57 +0100

    ex1''

--- week1/Day1/Exercises/Ex1.py
+++ week1/Day1/Exercises/Ex1.py
@@ -23,3 +23,24 @@ my_fav_numbers.pop()
 friend_fav_numbers= {6,1,9}
 our_fav_numbers = my_fav_numbers.union(friend_fav_numbers)
 print(our_fav_numbers)
+# 6
+# No, tuples are immutable, so you can't add integers directly to them. 
+# You can create a new tuple by combining the old one with new values.
+# 7
+basket = ["Banana", "Apples", "Oranges", "Blueberries"]
+basket.remove("Banana")
+basket.remove("Blueberries")
+basket.append("Kiwi")
+basket.insert(0, "Apples")
+apple_count = basket.count("Apples")
+print("Number of Apples:", apple_count)
+basket.clear()
+print(basket)
+# 8
+sandwich_orders = ["Tuna sandwich", "Pastrami sandwich", "Avocado sandwich", "Pastrami sandwich", "Egg sandwich", "Chicken sandwich", "Pastrami sandwich"]
+
+while "Pastrami sandwich" in sandwich_orders:
+    sandwich_orders.remove("Pastrami sandwich")
+
+finished_sandwiches = []
+
